38fc89e643549788db20125434840eb9d396bcf2
Mostafa Abbas <124041639+Mostafa-Abbas13@users.noreply.github.com>
2023-02-19 13:47:47 +0200

    Delete work.js

--- js/work.js
+++
@@ -1,9 +0,0 @@
-let inputs=document.querySelectorAll("input")
-function valid() {
- for (let i = 0; i < inputs.length; i++) {
-    const element = inputs[i];
-    if(element==""){
-        element.nextElementSibling.textContent=`${element.name} is `
-    }
- }
-}
